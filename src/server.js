fbe72e3c38c70e46780b61c7258eb596b554ca98
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-13 21:55:36 +0530

    update:server

--- src/server.js
+++ src/server.js
@@ -51,10 +51,10 @@ export function makeServer({ environment = "development" } = {}) {
       server.logging = false;
       users.forEach((item) =>
         server.create("user", {
-          ...item,
           followers: [],
           following: [],
           bookmarks: [],
+          ...item,
         })
       );
       posts.forEach((item) => server.create("post", { ...item }));
