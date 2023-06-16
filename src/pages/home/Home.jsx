0ae63d3900fbb8a3dd93a031bb0d29e63cb6cf64
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-16 17:35:52 +0530

    style:home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,6 +12,25 @@ export const Home = () => {
         <TopNavbar />
         <SideNavbar />
         <div className="feed-container">
+          <div className="create-post-container">
+            <div className="profile-input-container">
+              <div className="profile-picture"></div>
+              <input type="text" placeholder="what's happening?" />
+            </div>
+            <div className="create-post-buttons">
+              <button>Add photo</button>
+              <button>post</button>
+            </div>
+          </div>
+          <div className="user-feed-filter-container">
+            <p>Trending Post</p>
+            <img
+              src="https://ik.imagekit.io/u6itcrvxy/Social-Media-icons/filters-2-svgrepo-com__1_.svg?updatedAt=1686916215904"
+              alt="filters-logo"
+              width="25px"
+              height="25px"
+            />
+          </div>
           <h3>feed1</h3>
           <h3>feed2</h3>
           <h3>feed3</h3>
