165461946c5eae106a72533f8b826a870e70a4d6
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-21 11:32:51 +0530

    style:post card

---
+++ src/components/post-card/PostCard.jsx
@@ -0,0 +1,37 @@
+import "./postCard.css";
+
+export const PostCard = ({ post }) => {
+  const { fullName, username, content, postImage, createdAt } = post;
+  console.log("post-card", createdAt);
+  return (
+    <>
+      <div className="post-main-container">
+        <div className="post-head">
+          <div className="user-picture"></div>
+          <div className="user-name-username">
+            <div className="user-name">{fullName}</div>
+            <div className="user-username">{username}</div>
+          </div>
+          <div className="post-date">{createdAt}</div>
+        </div>
+        <div className="post-content-container">
+          <div className="post-content">{content}</div>
+          {postImage && (
+            <img src={postImage} alt={content} className="post-image" />
+          )}
+        </div>
+        <div className="post-foot">
+          <div className="post-like">
+            <img src="../icons/like.svg" alt="like-icon" />
+          </div>
+          <div className="post-comment">
+            <img src="../icons/comment.svg" alt="comment-logo" />
+          </div>
+          <div className="post-bookmark">
+            <img src="../icons/bookmark.svg" alt="bookmark-logo" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,8 +4,25 @@ import { BottomNavbar } from "../../components/navbar/BottomNavbar";
 import { TopNavbar } from "../../components/navbar/TopNavbar";
 import { SideNavbar } from "../../components/navbar/SideNavbar";
 import { SuggestionTab } from "../../components/suggestion-tab/SuggestionTab";
+import { PostCard } from "../../components/post-card/PostCard";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPost = async () => {
+      try {
+        const response = await fetch("/api/posts");
+        const data = await response.json();
+        console.log(data.posts);
+        setPosts(data.posts);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getPost();
+  }, []);
   return (
     <>
       <div className="home-main-container">
@@ -31,31 +48,9 @@ export const Home = () => {
               height="25px"
             />
           </div>
-          <h3>feed1</h3>
-          <h3>feed2</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
-          <h3>feed3</h3>
+          {posts.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
         <SuggestionTab />
         <BottomNavbar />
