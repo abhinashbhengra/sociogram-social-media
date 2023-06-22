6c9f718d8e76a903188f0a589f76ce7eb4d1d572
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-22 15:48:56 +0530

    refactor:filters

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,15 +7,19 @@ import { SuggestionTab } from "../../components/suggestion-tab/SuggestionTab";
 import { PostCard } from "../../components/post-card/PostCard";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { filteredPosts } from "../../utils/filters/filters";
 
 export const Home = () => {
   const [posts, setPosts] = useState([]);
   const [postInput, setPostInput] = useState("");
   const [image, setImage] = useState(null);
+  const [sortBy, setSortBy] = useState("All");
   const { authState } = useContext(AuthContext);
   const { token } = authState;
   const { user } = authState;
   const { following } = user;
+
+  const [showFilter, setShowFilter] = useState(false);
 
   const followingUserPost = posts.filter((post) => {
     return following.some(
@@ -26,6 +30,8 @@ export const Home = () => {
   const userPost = posts?.filter((post) => post.username === user.username);
 
   const allPost = [...followingUserPost, ...userPost];
+
+  const sortedPost = filteredPosts(allPost, sortBy);
 
   const post = {
     content: postInput,
@@ -55,6 +61,16 @@ export const Home = () => {
     e.target.files[0] === undefined
       ? setImage(null)
       : setImage(URL.createObjectURL(e.target.files[0]));
+  };
+
+  const handleSortByValue = (value) => {
+    // console.log(value);
+    setSortBy(value);
+    setShowFilter(false);
+  };
+
+  const handleFiltersOption = () => {
+    setShowFilter(true);
   };
 
   useEffect(() => {
@@ -111,16 +127,53 @@ export const Home = () => {
             </div>
           </div>
           <div className="user-feed-filter-container">
-            <p>Trending Post</p>
+            <p>{sortBy} Post</p>
             <img
               src="https://ik.imagekit.io/u6itcrvxy/Social-Media-icons/filters-2-svgrepo-com__1_.svg?updatedAt=1686916215904"
               alt="filters-logo"
               width="25px"
               height="25px"
               style={{ cursor: "pointer" }}
+              onClick={handleFiltersOption}
             />
+            <div
+              className="filters-main-container"
+              style={{ display: showFilter ? "block" : "none" }}
+            >
+              <div
+                value="All"
+                className="filter-button"
+                onClick={() => handleSortByValue("All")}
+              >
+                All
+              </div>
+              <div
+                className="filter-button"
+                onClick={() => handleSortByValue("Trending")}
+              >
+                Trending
+              </div>
+              <div
+                className="filter-button"
+                onClick={() => handleSortByValue("Latest")}
+              >
+                Latest
+              </div>
+              <div
+                className="filter-button"
+                onClick={() => handleSortByValue("Oldest")}
+              >
+                Oldest
+              </div>
+              <img
+                className="filter-cancel-button"
+                src="./icons/cancel.svg"
+                alt="cancel"
+                onClick={() => setShowFilter(false)}
+              />
+            </div>
           </div>
-          {allPost?.reverse().map((post) => (
+          {sortedPost?.reverse().map((post) => (
             <PostCard key={post._id} post={post} />
           ))}
         </div>
