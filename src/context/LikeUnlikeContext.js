a220aeb33844c51f28c9037b5f8498d281701d7b
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-23 10:55:54 +0530

    update:LikeUnlike Context Added

---
+++ src/context/LikeUnlikeContext.js
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { likePost } from "../utils/like-unlike/likePost";
+import { unlikePost } from "../utils/like-unlike/unlikePost";
+
+export const LikeUnlikeContext = createContext({
+  likeUnlikeState: {},
+  likeUnlikeDispatch: () => {},
+});
+
+export const LikeUnlikeProvider = ({ children }) => {
+  const [likeUnlikeItems, setLikeUnlikeItems] = useState([]);
+  const { authState } = useContext(AuthContext);
+  const { token } = authState;
+
+  const likeThePost = async (postId) => {
+    const likedItems = await likePost(token, postId);
+    setLikeUnlikeItems(likedItems);
+  };
+
+  const unlikeThePost = async (postId) => {
+    const updatedItems = await unlikePost(token, postId);
+    setLikeUnlikeItems(updatedItems);
+  };
+
+  const value = {
+    likeUnlikeItems,
+    likeThePost,
+    unlikeThePost,
+  };
+  return <LikeUnlikeContext value={value}>{children}</LikeUnlikeContext>;
+};
