23328e6e284cf59da4cf50c4e05511af09fe6a57
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-23 10:11:35 +0530

    update:Bookmark Context added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+import { BookmarkProvider } from "./context/BookmarkContext";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <BookmarkProvider>
+          <App />
+        </BookmarkProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
