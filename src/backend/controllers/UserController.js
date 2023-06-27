419f858605f046b8a27bc5e3095fdc2743dd0582
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-27 18:24:57 +0530

    fix:remove console.log

--- src/backend/controllers/UserController.js
+++ src/backend/controllers/UserController.js
@@ -56,9 +56,9 @@ export const editUserHandler = function (schema, request) {
       );
     }
     const { userData } = JSON.parse(request.requestBody);
-    console.log(
-      userData && userData.username && userData.username !== user.username
-    );
+    // console.log(
+    //   userData && userData.username && userData.username !== user.username
+    // );
     if (userData && userData.username && userData.username !== user.username) {
       return new Response(
         404,
