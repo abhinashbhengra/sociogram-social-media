08c237ea8070911c16c075f719a222997ff8ab50
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-28 14:45:10 +0530

    update:Profile Component

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -4,10 +4,6 @@ import { BottomNavbar } from "../../components/navbar/BottomNavbar";
 import { TopNavbar } from "../../components/navbar/TopNavbar";
 import { SideNavbar } from "../../components/navbar/SideNavbar";
 import { SuggestionTab } from "../../components/suggestion-tab/SuggestionTab";
-import { PostCard } from "../../components/post-card/PostCard";
-import { useContext, useEffect, useState } from "react";
-import { PostContext } from "../../context/PostContext";
-import { AuthContext } from "../../context/AuthContext";
 import { ProfileTab } from "../../components/profile-tab/ProfileTab";
 
 export const Profile = () => {
