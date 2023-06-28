026bfdaabcb2ff4f9160dc7de1292ec968688bff
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-28 16:32:53 +0530

    fix:Profile Avator

--- src/pages/bookmark/Bookmark.jsx
+++ src/pages/bookmark/Bookmark.jsx
@@ -8,14 +8,32 @@ import { PostCard } from "../../components/post-card/PostCard";
 import { useContext, useEffect, useState } from "react";
 import { BookmarkContext } from "../../context/BookmarkContext";
 import { PostContext } from "../../context/PostContext";
+import { AuthContext } from "../../context/AuthContext";
 
 export const Bookmark = () => {
+  const [allUsers, setAllUsers] = useState([]);
+  const { authState } = useContext(AuthContext);
   const { bookmark } = useContext(BookmarkContext);
   const { posts } = useContext(PostContext);
+
+  const { user } = authState;
 
   const filteredPost = posts.filter((post) =>
     bookmark.some(({ _id }) => post._id === _id)
   );
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      try {
+        const response = await fetch("/api/users");
+        const data = await response.json();
+        setAllUsers(data.users);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getAllUsers();
+  }, []);
 
   return (
     <>
@@ -26,7 +44,16 @@ export const Bookmark = () => {
           {filteredPost?.length === 0 && <p>No Bookmarks</p>}
 
           {filteredPost?.map((post) => (
-            <PostCard key={post._id} post={post} />
+            <PostCard
+              key={post._id}
+              post={post}
+              profileAvatar={
+                post.username === user.username
+                  ? user?.profileAvatar
+                  : allUsers.find((user) => user.username === post.username)
+                      ?.profileAvatar
+              }
+            />
           ))}
         </div>
         <SuggestionTab />
