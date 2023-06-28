fa41a6c5a03ed1a19bcf4c47b323aa7d8457a432
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-06-28 16:29:20 +0530

    fix:remove console.log

--- src/components/navbar/SideNavbar.jsx
+++ src/components/navbar/SideNavbar.jsx
@@ -31,8 +31,6 @@ export const SideNavbar = () => {
   const { authState } = useContext(AuthContext);
   const { user } = authState;
 
-  console.log(user);
-
   return (
     <>
       <div className="side-nav-main-container">
