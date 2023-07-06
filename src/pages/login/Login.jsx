4127d012774b84226de8389ea0194b667b1c5ab2
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-07-06 22:19:42 +0530

    style:Login Page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,3 +1,4 @@
+import "./login.css";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
@@ -25,32 +26,46 @@ export const Login = () => {
   };
 
   return (
-    <>
-      <h1>This is Login page.</h1>
+    <div className="login-main-container">
+      <h1 style={{ color: "#fff" }}>Sociogram</h1>
       <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="username"
-          value={user.username}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, username: e.target.value }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={user.password}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, password: e.target.value }))
-          }
-        />
-        <button type="submit">Login</button>
-        <p>
-          Don't have account ?{" "}
-          <span onClick={() => navigate("/signup")}>Sign up</span>
-        </p>
-        <p onClick={handleGuestLogin}>Add Guest Credentials</p>
+        <div>
+          <input
+            type="text"
+            placeholder="username"
+            value={user.username}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, username: e.target.value }))
+            }
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            value={user.password}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, password: e.target.value }))
+            }
+          />
+        </div>
+
+        <button type="submit">Log In</button>
+        <div className="signup-guest-container">
+          <p>
+            Don't have account ?{" "}
+            <span
+              onClick={() => navigate("/signup")}
+              style={{ color: "#0098fd", cursor: "pointer" }}
+            >
+              Sign up
+            </span>
+          </p>
+          <p onClick={handleGuestLogin} style={{ cursor: "pointer" }}>
+            Add Guest Credentials
+          </p>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
