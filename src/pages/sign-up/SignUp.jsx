657f8fffdb5ea82edc592fa72bbb263a3119ef77
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-07-06 22:34:04 +0530

    style:Sign Up  Page

--- src/pages/sign-up/SignUp.jsx
+++ src/pages/sign-up/SignUp.jsx
@@ -1,3 +1,4 @@
+import "./sign-up.css";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useNavigate } from "react-router-dom";
@@ -30,48 +31,66 @@ export const SignUp = () => {
   };
 
   return (
-    <>
-      <h1>This is sign up page.</h1>
+    <div className="signup-main-container">
+      <h1>Sociogram</h1>
       <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="First Name"
-          value={user.firstName}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, firstName: e.target.value }))
-          }
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={user.lastName}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, lastName: e.target.value }))
-          }
-        />
-        <input
-          type="text"
-          placeholder="username"
-          value={user.username}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, username: e.target.value }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="password"
-          value={user.password}
-          onChange={(e) =>
-            setUser((curr) => ({ ...curr, password: e.target.value }))
-          }
-        />
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={user.firstName}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, firstName: e.target.value }))
+            }
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={user.lastName}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, lastName: e.target.value }))
+            }
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="username"
+            value={user.username}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, username: e.target.value }))
+            }
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="password"
+            value={user.password}
+            onChange={(e) =>
+              setUser((curr) => ({ ...curr, password: e.target.value }))
+            }
+          />
+        </div>
+
         <button type="submit">Sign up</button>
-        <p>
-          Already have account ?{" "}
-          <span onClick={() => navigate("/login")}>login</span>
-        </p>
-        <p onClick={handleDummyDetails}>Dummy Details</p>
+        <div>
+          <p>
+            Already have account ?{" "}
+            <span
+              onClick={() => navigate("/login")}
+              style={{ color: "#0098fd", cursor: "pointer" }}
+            >
+              login
+            </span>
+          </p>
+          <p onClick={handleDummyDetails} style={{ cursor: "pointer" }}>
+            Fill Dummy Details
+          </p>
+        </div>
       </form>
-    </>
+    </div>
   );
 };
