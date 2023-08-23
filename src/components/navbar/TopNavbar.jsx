e034560e3c9b073cd61119dcf797500dac682e9a
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-23 09:07:51 +0530

    navbar style update

--- src/components/navbar/TopNavbar.jsx
+++ src/components/navbar/TopNavbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export const TopNavbar = () => {
@@ -5,11 +6,13 @@ export const TopNavbar = () => {
     <>
       <div className="top-nav-main-container">
         <div className="top-nav-logo-container">
-          <p style={{ color: "#a5a5a5" }}>Sociogram</p>
+          <Link to="/" className="navLink">
+            <p style={{ color: "#a5a5a5" }}>Sociogram</p>
+          </Link>
         </div>
-        <div className="top-nav-search-container">
+        {/* <div className="top-nav-search-container">
           <input type="text" placeholder="Search Users" />
-        </div>
+        </div> */}
       </div>
     </>
   );
