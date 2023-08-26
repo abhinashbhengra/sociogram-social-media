cff759335f8b4a14e56500557253357e4f0e77ca
Abhinash Bhengra <abhinashbhengra170dx@gmail.com>
2023-08-26 15:20:09 +0530

    fix:search component

--- src/components/search-result/SearchResult.jsx
+++ src/components/search-result/SearchResult.jsx
@@ -15,21 +15,21 @@ export const SearchResult = ({ user }) => {
           <p>{user.username}</p>
         </div>
       </div> */}
-      <div className="suggestion-container" key={user._id}>
-        <div
-          className="suggestion-user-picture"
-          onClick={() => navigate(`/profile/${user.username}`)}
-        >
-          <img src={user.profileAvatar} alt={user.username} />
-        </div>
-        <div
-          className="suggestion-user-name-username"
-          onClick={() => navigate(`/profile/${user.username}`)}
-        >
-          <div className="suggestion-user-name search-fullname">
-            {user.fullName}
+      <div className="suggestion-container searched-user" key={user._id}>
+        <div className="picture-username-fullname">
+          <div
+            className="suggestion-user-picture"
+            onClick={() => navigate(`/profile/${user.username}`)}
+          >
+            <img src={user.profileAvatar} alt={user.username} />
           </div>
-          <div className="suggestion-user-username">{user.username}</div>
+          <div
+            className="suggestion-user-name-username"
+            onClick={() => navigate(`/profile/${user.username}`)}
+          >
+            <div className="suggestion-user-name">{user.fullName}</div>
+            <div className="suggestion-user-username">{user.username}</div>
+          </div>
         </div>
       </div>
     </>
